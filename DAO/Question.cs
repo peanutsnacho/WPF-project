cbd3976728250bbac6bd004c24df0e6493760d34
gosia <malgorzata.fadziel@gmail.com>
2016-10-03 19:22:34 +0200

    added classes for abstract objects (test, question, answer)

---
+++ DAO/Question.cs
@@ -0,0 +1,84 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class Question : IQuestion
+    {
+        private int _questionID;
+        private string _text;
+        private int _points;
+        private List<IAnswer> _answers;
+        private List<IAnswer> _correctAnswers;
+        
+
+        public int QuestionID
+        {
+            get
+            {
+                return _questionID;
+            }
+
+            set
+            {
+                _questionID = value;
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+
+            set
+            {
+                _text = value;
+            }
+        }
+
+        public int Points
+        {
+            get
+            {
+                return _points;
+            }
+
+            set
+            {
+                _points = value;
+            }
+        }
+
+        public IEnumerable<IAnswer> Answers
+        {
+            get
+            {
+                return _answers;
+            }
+
+            set
+            {
+                _answers = value.ToList<IAnswer>();
+            }
+        }
+
+        public IEnumerable<IAnswer> CorrectAnswers
+        {
+            get
+            {
+                return _correctAnswers;
+            }
+
+            set
+            {
+                _correctAnswers = value.ToList<IAnswer>();
+            }
+        }
+    }
+}
